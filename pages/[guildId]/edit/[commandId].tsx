403c28a2071916398cc8b1139781e0d525def083
Will Nelson <will@wnelson.xyz>
2021-11-06 15:47:25 -0700

    Support editing guild commands

--- pages/[guildId]/edit/[commandId].tsx
+++ pages/[guildId]/edit/[commandId].tsx
@@ -1,3 +1,33 @@
+import { Container } from '@mui/material';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+import ApplicationCommand, { CommandFields } from '../../../src/components/ApplicationCommand';
+import fetcher, { fetch } from '../../../src/fetcher';
+
 export default function EditGuildCommand() {
-	return <div></div>;
+	const router = useRouter();
+	const { guildId, commandId } = router.query;
+
+	const { data: me } = useSWR('/oauth2/@me', fetcher);
+	const { data: command, mutate } = useSWR(`/applications/${me?.application?.id}/guilds/${guildId}/commands/${commandId}`, fetcher);
+
+	const saveCommand = async (command: CommandFields) => {
+		await fetch(`/applications/${me.application.id}/guilds/${guildId}/commands/${commandId}`, {
+			method: 'PATCH',
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify(command),
+		});
+
+		mutate(command);
+		router.back();
+	};
+
+	console.log(command);
+
+	return (
+		<Container>
+			{command ? <ApplicationCommand command={command} onSave={saveCommand} /> : "Loading"}
+		</Container>
+	);
 }
