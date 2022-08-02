cad2c0b3f810695b14c1ffa2bc61ed88e884afa1
Will Nelson <will@wnelson.xyz>
2022-08-01 20:15:09 -0700

    Show error when saving command

--- pages/edit/[commandId].tsx
+++ pages/edit/[commandId].tsx
@@ -1,5 +1,6 @@
-import { Container } from '@mui/material';
+import { Alert, Container, Snackbar } from '@mui/material';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import useSWR from 'swr';
 
 import ApplicationCommand, { CommandFields } from '../../src/components/ApplicationCommand';
@@ -12,22 +13,37 @@ export default function EditCommand() {
 	const { data: me } = useSWR('/oauth2/@me', fetcher);
 	const { data: command, mutate } = useSWR(`/applications/${me?.application?.id}/commands/${commandId}`, fetcher);
 
-	const saveCommand = async (command: CommandFields) => {
-		await fetch(`/applications/${me.application.id}/commands/${commandId}`, {
-			method: 'PATCH',
-			headers: { 'content-type': 'application/json' },
-			body: JSON.stringify(command),
-		});
+	const [error, setError] = useState<Error | null>(null);
 
-		mutate(command);
-		router.back();
+	const saveCommand = async (command: CommandFields) => {
+		try {
+			const res = await fetch(`/applications/${me.application.id}/commands/${commandId}`, {
+				method: 'PATCH',
+				headers: { 'content-type': 'application/json' },
+				body: JSON.stringify(command),
+			});
+
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
+
+			mutate(command);
+			router.back();
+		} catch (e) {
+			setError(e as Error);
+		}
 	};
 
-	console.log(command);
+	const handleClose = () => {
+		setError(null);
+	};
 
 	return (
 		<Container>
 			{command ? <ApplicationCommand command={command} onSave={saveCommand} /> : "Loading"}
+			<Snackbar open={error !== null} onClose={handleClose}>
+				<Alert elevation={3} variant='filled' severity='error' onClose={handleClose}>{error?.message ?? 'Something went wrong!'}</Alert>
+			</Snackbar>
 		</Container>
 	);
 }
