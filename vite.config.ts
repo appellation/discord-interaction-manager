867832271b186d09396dc19fa5878b27cef04721
Will Nelson <me@wnelson.dev>
2023-11-18 20:08:32 -0800

    Scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react-swc";
+import { presetUno } from "unocss";
+import unoCss from "unocss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		unoCss({
+			presets: [presetUno()],
+		}),
+		react(),
+	],
 });
