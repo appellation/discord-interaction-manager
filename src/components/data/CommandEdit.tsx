5942e9f0278fcd82f477979a4571aba96be4081f
Will Nelson <me@wnelson.dev>
2024-10-10 22:30:10 -0700

    Command edit component

---
+++ src/components/data/CommandEdit.tsx
@@ -0,0 +1,67 @@
+import { useForm } from "@tanstack/react-form";
+import type { APIApplicationCommand } from "discord-api-types/v10";
+import { CheckboxField } from "../ui/checkbox";
+import { TextInputField } from "../ui/text-input";
+import { Textarea } from "../ui/textarea";
+
+export default function CommandEdit({
+	data,
+}: {
+	readonly data?: APIApplicationCommand;
+}) {
+	const { Field } = useForm({
+		defaultValues: data,
+	});
+
+	return (
+		<form>
+			<Field name="name">
+				{(field) => <TextInputField field={field} label="Name" type="text" />}
+			</Field>
+			{data?.guild_id && (
+				<Field name="guild_id">
+					{(field) => (
+						<TextInputField
+							disabled
+							field={field}
+							label="Guild ID"
+							type="text"
+						/>
+					)}
+				</Field>
+			)}
+			<Field name="description">
+				{(field) => (
+					<Textarea
+						name={field.name}
+						onBlur={field.handleBlur}
+						onChange={(error) => field.handleChange(error.target.value)}
+						value={field.state.value}
+					/>
+				)}
+			</Field>
+			<Field name="default_member_permissions">
+				{(field) => (
+					<TextInputField
+						field={field}
+						label="Default Member Permissions"
+						type="text"
+					/>
+				)}
+			</Field>
+			<div className="flex items-center gap-2">
+				<Field name="dm_permission">
+					{(field) => <CheckboxField field={field} label="DM Permission" />}
+				</Field>
+				<Field name="default_permission">
+					{(field) => (
+						<CheckboxField field={field} label="Default Permission" />
+					)}
+				</Field>
+				<Field name="nsfw">
+					{(field) => <CheckboxField field={field} label="NSFW" />}
+				</Field>
+			</div>
+		</form>
+	);
+}
--- src/pages/commands/Edit.tsx
+++ src/pages/commands/Edit.tsx
@@ -1,10 +1,7 @@
-import { useForm } from "@tanstack/react-form";
 import { useQuery } from "@tanstack/react-query";
 import type { RESTGetAPIApplicationCommandResult } from "discord-api-types/v10";
 import { useParams } from "wouter";
-import { CheckboxField } from "~/components/ui/checkbox";
-import { TextInputField } from "~/components/ui/text-input";
-import { Textarea } from "~/components/ui/textarea";
+import CommandEdit from "~/components/data/CommandEdit";
 import { useQueryKey } from "~/lib/fetch";
 import { useCurrentApp } from "~/lib/state";
 
@@ -21,59 +18,10 @@ export default function EditCommand() {
 		queryKey,
 		enabled: currentApp != null,
 	});
-	const { Field } = useForm({
-		defaultValues: data,
-	});
 
 	return (
 		<main className="container mx-auto">
-			<form>
-				<Field name="name">
-					{(field) => <TextInputField field={field} label="Name" type="text" />}
-				</Field>
-				<Field name="guild_id">
-					{(field) => (
-						<TextInputField
-							disabled
-							field={field}
-							label="Guild ID"
-							type="text"
-						/>
-					)}
-				</Field>
-				<Field name="description">
-					{(field) => (
-						<Textarea
-							name={field.name}
-							onBlur={field.handleBlur}
-							onChange={(error) => field.handleChange(error.target.value)}
-							value={field.state.value}
-						/>
-					)}
-				</Field>
-				<Field name="default_member_permissions">
-					{(field) => (
-						<TextInputField
-							field={field}
-							label="Default Member Permissions"
-							type="text"
-						/>
-					)}
-				</Field>
-				<div className="flex items-center gap-2">
-					<Field name="dm_permission">
-						{(field) => <CheckboxField field={field} label="DM Permission" />}
-					</Field>
-					<Field name="default_permission">
-						{(field) => (
-							<CheckboxField field={field} label="Default Permission" />
-						)}
-					</Field>
-					<Field name="nsfw">
-						{(field) => <CheckboxField field={field} label="NSFW" />}
-					</Field>
-				</div>
-			</form>
+			<CommandEdit data={data} />
 		</main>
 	);
 }
