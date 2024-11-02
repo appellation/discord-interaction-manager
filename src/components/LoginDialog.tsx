73c02f8672ca8c3dabc0a1cf0b62dbc540da398b
Will Nelson <me@wnelson.dev>
2024-11-02 15:02:09 -0700

    Fix 1password closing dialog

--- src/components/LoginDialog.tsx
+++ src/components/LoginDialog.tsx
@@ -20,11 +20,25 @@ export function LoginDialog({
 		(newOpen: boolean) => setOpen(newOpen),
 		[],
 	);
+	const handleInteractOutside = useCallback(
+		(
+			event:
+				| CustomEvent<{ originalEvent: FocusEvent }>
+				| CustomEvent<{ originalEvent: PointerEvent }>,
+		) => {
+			const el = event.target as HTMLElement;
+			const is1Pass = el.localName.startsWith("com-1password");
+			if (is1Pass) {
+				event.preventDefault();
+			}
+		},
+		[],
+	);
 
 	return (
 		<Dialog onOpenChange={handleOpenChange} open={open}>
 			<DialogTrigger asChild={asChild}>{children}</DialogTrigger>
-			<DialogContent>
+			<DialogContent onInteractOutside={handleInteractOutside}>
 				<Suspense>
 					<LoginForm onSuccess={() => handleOpenChange(false)} />
 				</Suspense>
