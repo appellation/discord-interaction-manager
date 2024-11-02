01588495d3371b756cf7a1a581ef20a78fc360b5
Will Nelson <me@wnelson.dev>
2024-11-02 15:06:41 -0700

    Fix heading styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
+import { Link } from "wouter";
 import LoginMenu from "./LoginMenu";
+import { Separator } from "./ui/separator";
 import { Heading } from "./ui/typography";
 
 export default function Header() {
 	return (
-		<div className="flex items-baseline">
-			<Heading className="grow" level={1}>
-				Discord Interactions
-			</Heading>
-			<LoginMenu />
+		<div className="mb-8">
+			<div className="container mx-auto flex items-end pb-4">
+				<Heading className="grow" level={1}>
+					<Link to="/">Discord Interactions</Link>
+				</Heading>
+				<LoginMenu />
+			</div>
+			<Separator />
 		</div>
 	);
 }
