ea8f9bda134521b2663b1af9ea3515e36d828bda
Will Nelson <me@wnelson.dev>
2024-11-11 10:48:42 -0800

    Placeholder footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import Routes from "./Routes";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { queryClient } from "./lib/fetch";
 
@@ -13,6 +14,7 @@ export default function App() {
 			<QueryClientProvider client={queryClient}>
 				<Header />
 				<Routes />
+				<Footer />
 			</QueryClientProvider>
 		</StrictMode>
 	);
---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+	return <div className="mb-8" />;
+}
